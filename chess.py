c5c3f02d980377898130361b53159fd0e99f9977
Marlon Moser <marlon.moser@adfinis.com>
2024-05-28 18:18:40 +0200

    sync

--- chess.py
+++ chess.py
@@ -9,6 +9,11 @@ class Game:
     __board = None
     current_player = None
     black_castling_kingside = black_castling_queenside = white_castling_kingside = white_castling_queenside = True
+    last_move = None
+    moves_since_last_significant = 0
+    game_over = False
+    result = None
+    end_message = None
 
     def __init__(self):
         # Initialize the chess game
@@ -32,7 +37,7 @@ class Game:
                 if (x, (7-y)) in valid_moves:
                     print("\033[48;2;0;255;0m", end="")
                     pass
-                elif (x + y) % 2 == 0:
+                elif (x + y) % 2 == 1:
                     print("\033[48;2;215;135;0m", end="")
                     pass
                 else:
@@ -45,16 +50,21 @@ class Game:
                 print("\033[m", end="")
             print(f" {8-y}")
         print("  A B C D E F G H")
+        if self.moves_since_last_significant >= 100:
+            print("50 or more unsignificant moves passed. Press (r) for remis or continue playing.")
     
-    def move_piece(self, x: int, y: int, target_x: int, target_y: int):
+    def move_piece(self, x: int, y: int, target_x: int, target_y: int, sim: bool = False):
         """Move a piece to a new position"""
         piece = self.get_piece_at(x, y)
         self.__board[y][x] = None
         # add support for pawn promotion
         if isinstance(piece, Pawn) and ((piece.team == "white" and target_y == 7) or (piece.team == "black" and target_y == 0)):
             while True:
-                print("Which piece do you want to promote to? (q, r, b, n): ")
-                wish = get_key_press()
+                if sim:
+                    wish = "q"
+                else:
+                    print("Which piece do you want to promote to? (q, r, b, n): ")
+                    wish = get_key_press()
                 if re.match(r"^[qrbn]$", wish):
                     break
             match wish:
@@ -66,41 +76,43 @@ class Game:
                     piece = Bishop(self, piece.team, target_x, target_y)
                 case "n":
                     piece = Knight(self, piece.team, target_x, target_y)
+        # add support for en passant
+        if isinstance(piece, Pawn) and x != target_x and self.get_piece_at(target_x, target_y) is None:
+            self.__board[y][target_x] = None
         # add support for castling
         if isinstance(piece, King):
             if piece.team == "white":
-                self.white_castling_queenside = self.white_castling_kingside = False
+                if not sim:
+                    self.white_castling_queenside = self.white_castling_kingside = False
                 if target_x == 6 and x == 4:
                     rook = self.get_piece_at(7, 0)
-                    if self.__board[0][5] is None and self.__board[0][6] is None:
-                        self.__board[0][7] = None
-                        self.__board[0][5] = rook
-                        rook.x = 5
-                        rook.y = 0
+                    self.__board[0][7] = None
+                    self.__board[0][5] = rook
+                    rook.x = 5
+                    rook.y = 0
                 elif target_x == 2 and x == 4:
                     rook = self.get_piece_at(0, 0)
-                    if self.__board[0][1] is None and self.__board[0][2] is None and self.__board[0][3] is None:
-                        self.__board[0][0] = None
-                        self.__board[0][3] = rook
-                        rook.x = 3
-                        rook.y = 0
+                    self.__board[0][0] = None
+                    self.__board[0][3] = rook
+                    rook.x = 3
+                    rook.y = 0
             elif piece.team == "black":
-                self.black_castling_queenside = self.black_castling_kingside = False
+                if not sim:
+                    self.black_castling_queenside = self.black_castling_kingside = False
                 if target_x == 6 and x == 4:
                     rook = self.get_piece_at(7, 7)
-                    if self.__board[7][5] is None and self.__board[7][6] is None:
-                        self.__board[7][7] = None
-                        self.__board[7][5] = rook
-                        rook.x = 5
-                        rook.y = 7
+                    self.__board[7][7] = None
+                    self.__board[7][5] = rook
+                    rook.x = 5
+                    rook.y = 7
                 elif target_x == 2 and x == 4:
                     rook = self.get_piece_at(0, 7)
-                    if self.__board[7][1] is None and self.__board[7][2] is None and self.__board[7][3] is None:
-                        self.__board[7][0] = None
-                        self.__board[7][3] = rook
-                        rook.x = 3
-                        rook.y = 7
-            # dissalow castling if rook gets moved or taken
+                    self.__board[7][0] = None
+                    self.__board[7][3] = rook
+                    rook.x = 3
+                    rook.y = 7
+        # dissalow castling if rook gets moved or taken
+        if not sim:
             if (piece.x == 0 and piece.y == 0) or (target_x == 0 and target_y == 0):
                 self.white_castling_queenside = False
             elif (piece.x == 7 and piece.y == 0) or (target_x == 7 and target_y == 0):
@@ -109,12 +121,17 @@ class Game:
                 self.black_castling_queenside = False
             elif (piece.x == 7 and piece.y == 7) or (target_x == 7 and target_y == 7):
                 self.black_castling_kingside = False
+            if isinstance(piece, Pawn) or self.get_piece_at(target_x, target_y) is not None:
+                self.moves_since_last_significant = 0
 
         self.__board[target_y][target_x] = piece
         piece.x = target_x
         piece.y = target_y
-        self.render_board()
-        self.current_player = "black" if self.current_player == "white" else "white"
+        if not sim:
+            self.current_player = "black" if self.current_player == "white" else "white"
+            self.render_board()
+            self.last_move = (x, y, target_x, target_y)
+            self.moves_since_last_significant += 1
 
     def is_check(self, player: str) -> bool:
         """Check if the specified player is in check"""
@@ -131,7 +148,7 @@ class Game:
                             king_position = (x, y)
                     else:
                         opponent_pieces.append(piece)
-        
+
         # Check if any opponent's piece can attack the king
         for piece in opponent_pieces:
             valid_moves = piece.get_valid_moves()
@@ -139,6 +156,21 @@ class Game:
                 return True
         
         return False
+
+    def is_check_after_move(self, x: int, y: int, target_x: int, target_y: int) -> bool:
+        """Check if the current player is in check after a move"""
+        piece = self.get_piece_at(x, y)
+        target_piece = self.get_piece_at(target_x, target_y)
+        self.__board[y][x] = None
+        self.__board[target_y][target_x] = piece
+        piece.x = target_x
+        piece.y = target_y
+        is_check = self.is_check(self.current_player)
+        self.__board[y][x] = piece
+        self.__board[target_y][target_x] = target_piece
+        piece.x = x
+        piece.y = y
+        return is_check
 
     def create_board(self):
         """Create and return the initial chess board"""
@@ -150,14 +182,14 @@ class Game:
              [None, None, None, None, None, None, None, None],
              [Pawn(self, "black", 0, 6), Pawn(self, "black", 1, 6), Pawn(self, "black", 2, 6), Pawn(self, "black", 3, 6), Pawn(self, "black", 4, 6), Pawn(self, "black", 5, 6), Pawn(self, "black", 6, 6), Pawn(self, "black", 7, 6)],
              [Rook(self, "black", 0, 7), Knight(self, "black", 1, 7), Bishop(self, "black", 2, 7), Queen(self, "black", 3, 7), King(self, "black", 4, 7), Bishop(self, "black", 5, 7), Knight(self, "black", 6, 7), Rook(self, "black", 7, 7)]]
-        #self.__board = [[None, None, None, None, None, None, None, None],
-        #     [None, Pawn(self, "white", 1, 6), None, None, None, King(self, "Black", 5, 6), None, None],
+        #self.__board = [[Rook(self, "white", 0, 0), None, None, None, King(self, "white", 4, 0), None, None, Rook(self, "white", 7, 0)],
+        #     [None, Pawn(self, "white", 1, 1), None, None, None, None, None, None],
         #     [None, None, None, None, None, None, None, None],
         #     [None, None, None, None, None, None, None, None],
-        #     [None, None, None, None, None, None, None, None],
+        #     [None, None, None, None, None, None, King(self, "black", 5, 4), None],
         #     [None, None, None, None, None, None, None, None],
         #     [Pawn(self, "black", 0, 6), None, None, None, None, None, None, None],
-        #     [None, None, None, None, None, King(self, "white", 5, 0), None, None]]
+        #     [None, None, None, None, None, None, None, None]]
 
 class Piece:
     game: Game
@@ -181,6 +213,11 @@ class Piece:
             if piece is not None and piece.team == self.team:
                 continue
             valid_moves.append(move)
+        #cache = valid_moves
+        #valid_moves = []
+        #for move in cache:
+        #    if self.game.is_check_after_move(self.x, self.y, move[0], move[1]):
+        #        valid_moves.append(move)
         return valid_moves
 
     def get_game(self) -> Game:
@@ -204,6 +241,12 @@ class Pawn(Piece):
                 valid_moves.append((self.x - 1, self.y + 1))
             if self.x + 1 < 8 and self.y + 1 < 8 and self.game.get_piece_at(self.x + 1, self.y + 1) is not None:
                 valid_moves.append((self.x + 1, self.y + 1))
+            # En passant
+            if self.y == 4:
+                if self.x - 1 >= 0 and self.game.get_piece_at(self.x - 1, self.y) is not None and isinstance(self.game.get_piece_at(self.x - 1, self.y), Pawn) and self.game.last_move == (self.x - 1, self.y + 2, self.x - 1, self.y):
+                    valid_moves.append((self.x - 1, self.y + 1))
+                if self.x + 1 < 8 and self.game.get_piece_at(self.x + 1, self.y) is not None and isinstance(self.game.get_piece_at(self.x + 1, self.y), Pawn) and self.game.last_move == (self.x + 1, self.y + 2, self.x + 1, self.y):
+                    valid_moves.append((self.x + 1, self.y + 1))
         else:
             # Move one forward
             if self.y - 1 >= 0 and self.game.get_piece_at(self.x, self.y - 1) is None:
@@ -216,6 +259,12 @@ class Pawn(Piece):
                 valid_moves.append((self.x - 1, self.y - 1))
             if self.x + 1 < 8 and self.y - 1 >= 0 and self.game.get_piece_at(self.x + 1, self.y - 1) is not None:
                 valid_moves.append((self.x + 1, self.y - 1))
+            # En passant
+            if self.y == 3:
+                if self.x - 1 >= 0 and self.game.get_piece_at(self.x - 1, self.y) is not None and isinstance(self.game.get_piece_at(self.x - 1, self.y), Pawn) and self.game.last_move == (self.x - 1, self.y - 2, self.x - 1, self.y):
+                    valid_moves.append((self.x - 1, self.y - 1))
+                if self.x + 1 < 8 and self.game.get_piece_at(self.x + 1, self.y) is not None and isinstance(self.game.get_piece_at(self.x + 1, self.y), Pawn) and self.game.last_move == (self.x + 1, self.y - 2, self.x + 1, self.y):
+                    valid_moves.append((self.x + 1, self.y - 1))
         return super().get_valid_moves(valid_moves)
 
 class Rook(Piece):
@@ -363,7 +412,7 @@ class King(Piece):
         for move in moves:
             if 0 <= move[0] < 8 and 0 <= move[1] < 8:
                 valid_moves.append(move)
-        # add castling
+        # castling
         if self.team == "white":
             if self.game.white_castling_kingside:
                 if self.game.get_piece_at(5, 0) is None and self.game.get_piece_at(6, 0) is None:
@@ -392,7 +441,7 @@ def get_key_press():
         # Reset terminal settings
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-def get_coords() -> Tuple[int,int]:
+def get_coords(game: Game) -> Tuple[int,int]:
     x = y = None
     while x is None or y is None:
         move_input = get_key_press()
@@ -400,31 +449,49 @@ def get_coords() -> Tuple[int,int]:
             x = ord(move_input[0].lower()) - 97
         elif re.match(r"^[1-8]$", move_input):
             y = int(move_input) - 1
+        elif move_input == "r" and game.moves_since_last_significant >= 100:
+            game.game_over = True
+            game.result = "remis"
+            game.end_message = "Game ended in a remis due to 50 moves rule."
+            return (0, 0)
+
     return (x, y)
     
-
-game = Game()
-game.create_board()
-game.render_board()
-
-while True:
-    # Get the source and target coordinates from the user
-    while True:
-        source_x, source_y = get_coords()
-        piece = game.get_piece_at(source_x, source_y)
-        if piece is not None and piece.team == game.current_player and piece.get_valid_moves() != []:
-            break
-        else:
-            print("Invalid move. Please enter a valid move.")
-
-    game.render_board(game.get_piece_at(source_x, source_y).get_valid_moves())
-    print(source_x, source_y)
-    while True:
-        target_x, target_y = get_coords()
-        if (target_x, target_y) in game.get_piece_at(source_x, source_y).get_valid_moves():
-            break
-        else:
-            print("Invalid move. Please enter a valid move.")
-
-    game.move_piece(source_x, source_y, target_x, target_y)
+def game_loop() -> Tuple[str, str]:
+    game = Game()
+    game.create_board()
     game.render_board()
+
+    while True:
+        # Get the source and target coordinates from the user
+        while True:
+            source_x, source_y = get_coords(game)
+            if game.game_over:
+                return (game.result, game.end_message)
+            piece = game.get_piece_at(source_x, source_y)
+            if piece is not None and piece.team == game.current_player and piece.get_valid_moves() != []:
+                break
+            else:
+                print("Invalid move. Please enter a valid move.")
+
+        game.render_board(game.get_piece_at(source_x, source_y).get_valid_moves())
+        print(source_x, source_y)
+        while True:
+            target_x, target_y = get_coords(game)
+            if game.game_over:
+                return (game.result, game.end_message)
+            if (target_x, target_y) in game.get_piece_at(source_x, source_y).get_valid_moves():
+                break
+            else:
+                print("Invalid move. Please enter a valid move.")
+
+        game.move_piece(source_x, source_y, target_x, target_y)
+        game.render_board()
+        if game.moves_since_last_significant >= 150:
+            game.game_over = True
+            game.result = "remis"
+            game.end_message = "Game ended in a remis due to 75 moves rule."
+            return (game.result, game.end_message)
+
+result, message = game_loop()
+print("game ended", result, message)
