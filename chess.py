ae7e2be3d6465681db85afc5334dbfb4168601fe
Marlon Moser <marlon.moser@adfinis.com>
2024-06-11 15:59:15 +0200

    DB yayyyy

--- chess.py
+++ chess.py
@@ -4,9 +4,11 @@ import sys
 import tty
 import termios
 import re
+import sqlite3
+import ast
 
 class Game:
-    __board: list = None
+    board: list = None
     current_player: str = None
     black_castling_kingside = black_castling_queenside = white_castling_kingside = white_castling_queenside = True
     last_move = None
@@ -17,12 +19,12 @@ class Game:
 
     def __init__(self):
         # Initialize the chess game
-        self.__board = self.create_board()
+        self.board = self.create_board()
         self.current_player = "white"
 
     def get_piece_at(self, x: int, y: int) -> 'Piece | None':
         """Return the piece at the specified x and y coordinates"""
-        return self.__board[y][x]
+        return self.board[y][x]
     
     # Render the board in the terminal
     def render_board(self, valid_moves: List[Tuple[int,int]] = []):
@@ -56,7 +58,7 @@ class Game:
     def move_piece(self, x: int, y: int, target_x: int, target_y: int, sim: bool = False):
         """Move a piece to a new position"""
         piece = self.get_piece_at(x, y)
-        self.__board[y][x] = None
+        self.board[y][x] = None
         # add support for pawn promotion
         if isinstance(piece, Pawn) and ((piece.team == "white" and target_y == 7) or (piece.team == "black" and target_y == 0)):
             while True:
@@ -78,7 +80,7 @@ class Game:
                     piece = Knight(self, piece.team, target_x, target_y)
         # add support for en passant
         if isinstance(piece, Pawn) and x != target_x and self.get_piece_at(target_x, target_y) is None:
-            self.__board[y][target_x] = None
+            self.board[y][target_x] = None
         # add support for castling
         if isinstance(piece, King):
             if piece.team == "white":
@@ -86,14 +88,14 @@ class Game:
                     self.white_castling_queenside = self.white_castling_kingside = False
                 if target_x == 6 and x == 4:
                     rook = self.get_piece_at(7, 0)
-                    self.__board[0][7] = None
-                    self.__board[0][5] = rook
+                    self.board[0][7] = None
+                    self.board[0][5] = rook
                     rook.x = 5
                     rook.y = 0
                 elif target_x == 2 and x == 4:
                     rook = self.get_piece_at(0, 0)
-                    self.__board[0][0] = None
-                    self.__board[0][3] = rook
+                    self.board[0][0] = None
+                    self.board[0][3] = rook
                     rook.x = 3
                     rook.y = 0
             elif piece.team == "black":
@@ -101,14 +103,14 @@ class Game:
                     self.black_castling_queenside = self.black_castling_kingside = False
                 if target_x == 6 and x == 4:
                     rook = self.get_piece_at(7, 7)
-                    self.__board[7][7] = None
-                    self.__board[7][5] = rook
+                    self.board[7][7] = None
+                    self.board[7][5] = rook
                     rook.x = 5
                     rook.y = 7
                 elif target_x == 2 and x == 4:
                     rook = self.get_piece_at(0, 7)
-                    self.__board[7][0] = None
-                    self.__board[7][3] = rook
+                    self.board[7][0] = None
+                    self.board[7][3] = rook
                     rook.x = 3
                     rook.y = 7
         # dissalow castling if rook gets moved or taken
@@ -124,7 +126,7 @@ class Game:
             if isinstance(piece, Pawn) or self.get_piece_at(target_x, target_y) is not None:
                 self.moves_since_last_significant = 0
 
-        self.__board[target_y][target_x] = piece
+        self.board[target_y][target_x] = piece
         piece.x = target_x
         piece.y = target_y
         if not sim:
@@ -161,13 +163,13 @@ class Game:
         """Check if the current player is in check after a move"""
         piece = self.get_piece_at(x, y)
         target_piece = self.get_piece_at(target_x, target_y)
-        self.__board[y][x] = None
-        self.__board[target_y][target_x] = piece
+        self.board[y][x] = None
+        self.board[target_y][target_x] = piece
         piece.x = target_x
         piece.y = target_y
         is_check = self.is_check(self.current_player, True)
-        self.__board[y][x] = piece
-        self.__board[target_y][target_x] = target_piece
+        self.board[y][x] = piece
+        self.board[target_y][target_x] = target_piece
         piece.x = x
         piece.y = y
         return is_check
@@ -183,7 +185,7 @@ class Game:
 
     def create_board(self):
         """Create and return the initial chess board"""
-        self.__board = [[Rook(self, "white", 0, 0), Knight(self, "white", 1, 0), Bishop(self, "white", 2, 0), Queen(self, "white", 3, 0), King(self, "white", 4, 0), Bishop(self, "white", 5, 0), Knight(self, "white", 6, 0), Rook(self, "white", 7, 0)],
+        self.board = [[Rook(self, "white", 0, 0), Knight(self, "white", 1, 0), Bishop(self, "white", 2, 0), Queen(self, "white", 3, 0), King(self, "white", 4, 0), Bishop(self, "white", 5, 0), Knight(self, "white", 6, 0), Rook(self, "white", 7, 0)],
              [Pawn(self, "white", 0, 1), Pawn(self, "white", 1, 1), Pawn(self, "white", 2, 1), Pawn(self, "white", 3, 1), Pawn(self, "white", 4, 1), Pawn(self, "white", 5, 1), Pawn(self, "white", 6, 1), Pawn(self, "white", 7, 1)],
              [None, None, None, None, None, None, None, None],
              [None, None, None, None, None, None, None, None],
@@ -191,14 +193,14 @@ class Game:
              [None, None, None, None, None, None, None, None],
              [Pawn(self, "black", 0, 6), Pawn(self, "black", 1, 6), Pawn(self, "black", 2, 6), Pawn(self, "black", 3, 6), Pawn(self, "black", 4, 6), Pawn(self, "black", 5, 6), Pawn(self, "black", 6, 6), Pawn(self, "black", 7, 6)],
              [Rook(self, "black", 0, 7), Knight(self, "black", 1, 7), Bishop(self, "black", 2, 7), Queen(self, "black", 3, 7), King(self, "black", 4, 7), Bishop(self, "black", 5, 7), Knight(self, "black", 6, 7), Rook(self, "black", 7, 7)]]
-        self.__board = [[Rook(self, "white", 0, 0), None, None, None, King(self, "white", 4, 0), None, None, Rook(self, "white", 7, 0)],
-             [None, Pawn(self, "white", 1, 1), None, None, None, None, None, None],
-             [None, None, None, None, None, None, None, None],
-             [None, None, None, None, None, None, None, None],
-             [None, None, None, None, None, None, King(self, "black", 6, 4), None],
-             [None, None, None, None, None, None, None, None],
-             [Pawn(self, "black", 0, 6), None, None, None, None, None, None, None],
-             [None, None, None, None, None, None, None, None]]
+        #self.board = [[Rook(self, "white", 0, 0), None, None, None, King(self, "white", 4, 0), None, None, Rook(self, "white", 7, 0)],
+        #     [None, Pawn(self, "white", 1, 1), None, None, None, None, None, None],
+        #     [None, None, None, None, None, None, None, None],
+        #     [None, None, None, None, None, None, None, None],
+        #     [None, None, None, None, None, None, King(self, "black", 6, 4), None],
+        #     [None, None, None, None, None, None, None, None],
+        #     [Pawn(self, "black", 0, 6), None, None, None, None, None, None, None],
+        #     [None, None, None, None, None, None, None, None]]
 
 class Piece:
     game: Game
@@ -467,7 +469,7 @@ def get_coords(game: Game) -> Tuple[int,int]:
 
     return (x, y)
     
-def game_loop() -> Tuple[str, str]:
+def game_loop() -> Tuple[Tuple[str, str], str]:
     game = Game()
     game.create_board()
     game.render_board()
@@ -477,7 +479,7 @@ def game_loop() -> Tuple[str, str]:
         while True:
             source_x, source_y = get_coords(game)
             if game.game_over:
-                return (game.result, game.end_message)
+                return ((game.result, None), game.end_message)
             piece = game.get_piece_at(source_x, source_y)
             if piece is not None and piece.team == game.current_player and piece.get_valid_moves() != []:
                 break
@@ -489,7 +491,7 @@ def game_loop() -> Tuple[str, str]:
         while True:
             target_x, target_y = get_coords(game)
             if game.game_over:
-                return (game.result, game.end_message)
+                return ((game.result, None), game.end_message)
             if (target_x, target_y) in game.get_piece_at(source_x, source_y).get_valid_moves():
                 break
             else:
@@ -502,19 +504,112 @@ def game_loop() -> Tuple[str, str]:
             if game.is_check(game.current_player):
                 game.game_over = True
                 game.result = "checkmate"
-                game.end_message = f"Checkmate! {game.current_player} wins!"
-                return (game.result, game.end_message)
+                winner = "white" if game.current_player == "black" else "black"
+                game.end_message = f"Checkmate! {winner} wins!"
+                return ((game.result, winner), game.end_message)
             else:
                 game.game_over = True
                 game.result = "remis"
                 game.end_message = "Game ended in a remis due to stalemate."
-                return (game.result, game.end_message)
+                return ((game.result, None), game.end_message)
 
         if game.moves_since_last_significant >= 150:
             game.game_over = True
             game.result = "remis"
             game.end_message = "Game ended in a remis due to 75 moves rule."
-            return (game.result, game.end_message)
+            return ((game.result, None), game.end_message)
 
-result, message = game_loop()
-print("game ended", result, message)
+def db_setup(db: sqlite3.Connection):
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS player (id INTEGER PRIMARY KEY, name TEXT, wins INTEGER, loses INTEGER, games INTEGER)")
+    db.commit()
+
+def db_check_player(db: sqlite3.Connection, name: str):
+    """Check if the player exists in the database and create a new entry if not"""
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM player WHERE name = ?", (name,))
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO player (name, wins, loses, games) VALUES (?, 0, 0, 0)", (name,))
+
+def db_update_player(db: sqlite3.Connection, white: str, black: str, result: Tuple[str, str]):
+    """Update the player statistics in the database"""
+    cursor = db.cursor()
+    if result[0] == "checkmate":
+        winner = white if result[1] == "white" else black
+        loser = white if result[1] == "black" else black
+        cursor.execute("UPDATE player SET wins = wins + 1 WHERE name = ?", (winner,))
+        cursor.execute("UPDATE player SET loses = loses + 1 WHERE name = ?", (loser,))
+    cursor.execute("UPDATE player SET games = games + 1 WHERE name = ? OR name = ?", (white, black))
+    db.commit()
+
+def db_get_statistics(db: sqlite3.Connection, name: str):
+    """Return the player statistics from the database"""
+    cursor = db.cursor()
+    cursor.execute("SELECT wins, loses, games FROM player WHERE name = ?", (name,))
+    if (row := cursor.fetchone()) is not None:
+        print("Statistics for player", name)
+        print("Wins: ", row[0])
+        print("Loses: ", row[1])
+        print("Draws: ", (row[2] - row[0] - row[1]))
+    else:
+        print("No player with that name found.")
+        return
+
+def export_game(game: Game) -> str:
+    board = game.board
+    str_array = [[None for _ in range(8)] for _ in range(8)]
+    for y in range(8):
+        for x in range(8):
+            piece = board[y][x]
+            if piece is not None:
+                match piece.__class__.__name__:
+                    case "Pawn":
+                        str_array[y][x] = 'p' if piece.team == "black" else 'P'
+                    case "Rook":
+                        str_array[y][x] = 'r' if piece.team == "black" else 'R'
+                    case "Knight":
+                        str_array[y][x] = 'n' if piece.team == "black" else 'N'
+                    case "Bishop":
+                        str_array[y][x] = 'b' if piece.team == "black" else 'B'
+                    case "Queen":
+                        str_array[y][x] = 'q' if piece.team == "black" else 'Q'
+                    case "King":
+                        str_array[y][x] = 'k' if piece.team == "black" else 'K'
+            else:
+                str_array[y][x] = 'e'
+    return str(str_array)
+
+db = sqlite3.connect("chess.db")
+db_setup(db)
+
+#os.system("clear")
+#print("#############################################")
+#print("# Welcome to Chess!                         #")
+#print("#############################################")
+#while True:
+#    print("What do you want to do?")
+#    print(" 1. Play a game")
+#    print(" 2. Show statistics")
+#    print(" q. Quit")
+#    match get_key_press():
+#        case "1":
+#            white = input("Enter the name of the white player: ")
+#            black = input("Enter the name of the black player: ")
+#            db_check_player(db, white)
+#            db_check_player(db, black)
+#            result, message = game_loop()
+#            print(message)
+#            db_update_player(db, white, black, result)
+#        case "2":
+#            name = input("Enter the name of the player you want to see the statistics for: ")
+#            db_get_statistics(db, name)
+#            pass
+#        case "q":
+#            break
+#db.close()
+#print("Thank you for playing!")
+
+game = Game()
+game.create_board()
+print(export_game(game))
+rook = Rook(game, "white", 0, 0)
